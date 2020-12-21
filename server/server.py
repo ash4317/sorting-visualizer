62bb58c21d3240634f95e02525647326602a0449
Ashwin Milind Kulkarni <ashwinkulkarni82@gmail.com>
2020-12-21 10:55:05 +0530

    Algo changes according to the dropdown algo Speed changing according to algo Bubblesort, selection, insertion done

---
+++ server/server.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from algos.bubblesort import bubblesort
+from algos.selectionsort import selectionsort
+from algos.insertionsort import insertionsort
+#from quicksort import main
+import random
+
+app = Flask(__name__, template_folder="../client/templates", static_folder="../client/static")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.htm")
+
+@app.route("/<algo>/<n>")
+def api(algo, n):
+    n = int(n)
+    arr = random.sample(range(1, n + 1), n)
+    if algo == "bubble":
+        data = bubblesort(arr)
+    elif algo == "insertion":
+        data = insertionsort(arr)
+    elif algo == "selection":
+        data = selectionsort(arr)
+    print(data)
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
